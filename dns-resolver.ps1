20ed7a1e0a8a7c25c04bbceaf866af48d5e3c7fd
Brandon-Duffy <97912287+Brandon-Duffy@users.noreply.github.com>
2025-02-03 19:09:58 -0500

    assignment 3.1

---
+++ dns-resolver.ps1
@@ -0,0 +1,14 @@
+﻿param (
+    [string]$network_prefix,
+    [string]$dns_server
+)
+
+
+for ($i = 1; $i -le 254; $i++) {
+    $ip = "$network_prefix.$i"
+    $hostResult = nslookup $ip $dns_server 2>$null | Select-String "Name:" | ForEach-Object { $_.ToString() -replace "Name:\s+", "" }
+
+    if ($hostResult) { 
+        Write-Host "$ip -> $hostResult"
+    }
+}
